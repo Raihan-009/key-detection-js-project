85040cfa80f4f860f8be94d0473a452d75cccf2b
Raihan Islam <64744693+Raihan-009@users.noreply.github.com>
2022-04-04 19:36:43 +0600

    key detected

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const background = document.querySelector(".background");
 document.addEventListener("keydown", (e) => {
+  // console.log(e);
   background.classList.add("active");
-  console.log(e);
+
+  let keyname = e.keyCode === 32 ? "Space" : e.keyCode;
+  background.querySelector(".key-code").innerHTML = e.key;
+  background.querySelector(".key-name").innerHTML = keyname;
+  background.querySelector(".key span").innerHTML = keyname;
+  background.querySelector(".code span").innerHTML = e.keyCode;
 });
